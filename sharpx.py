12b3d13da0faad3a65172e164ec19aa00e45c5bc
FRX011 <oop963014@gmail.com>
2024-10-19 08:17:37 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7010248454:AAF9aCEjE-9I88m8Pq5EDEnEfiOYM8AWrWM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Manager_149")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5436530930")
+
+
+
+
+
+  
